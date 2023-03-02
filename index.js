cb04cc6d3b8e2627995de0d46e5e4f907a8e4a97
Ripty Singla <ripty@Riptys-MacBook-Air.local>
2023-03-02 16:58:24 +0530

    express route

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routes/index");
 
+app.use("/", router);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
